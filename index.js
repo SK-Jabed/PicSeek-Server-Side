868929e0e0769e7a33014104c5bb18e2c6e8f527
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-07 23:44:10 +0600

    Routes for Image and Comment Functionalities Done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const app = require("./src/app");
+const { connectDB } = require("./src/utils/connectDB");
+
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`🚩 Server is Running at Port⚡ ${port}`);
+      console.log(`Connected to MongoDB `);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
-//definition
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const logger = require("./middleware/logger");
+const imageRouter = require("./routes/image.route");
+const commentRouter = require("./routes/comment.route");
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(logger);
+
+//Routes
+app.use("/api/v1/image", imageRouter);
+app.use("/api/v1/comment", commentRouter);
+
+app.get("/", (req, res) => {
+  res.send({
+    status: 200,
+    message: `🚩 Server is Running `,
+  });
+});
+
+module.exports = app;
