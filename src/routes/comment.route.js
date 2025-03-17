dc9d84f5d82e419d1c8b2f42392f374b6cbc39a7
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-17 23:48:39 +0600

    New middleware functionalities added to the logger js file

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { postUserComment } = require("../controllers/comment.controller");
 const commentRouter = express.Router();
 
+
+
+
 commentRouter.post("/create", postUserComment);
 
 module.exports = commentRouter;
