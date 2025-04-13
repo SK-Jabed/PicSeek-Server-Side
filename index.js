adf32259aac321961d0e5ba0afc13ed8032e52dc
Sheikh Jabed <jabedbd2295@gmail.com>
2025-04-13 23:29:22 +0600

    Necessary files formatted and updated

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
+require("dotenv").config();
 const app = require("./src/app");
 const { connectDB } = require("./src/utils/connectDB");
-
-require("dotenv").config();
-
 
 const port = process.env.PORT || 3000;
 
